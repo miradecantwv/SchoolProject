80aee5b97edf762646be6c2fe55163ac41fba9ea
miradecantwv <miradecantwv@gmx.com>
2025-03-06 16:36:46 +0000

    Add 3.rs

---
+++ src/3.rs
@@ -0,0 +1,13 @@
+
+fn main() {
+    let x = 10;
+    let y = 20;
+
+    if x > y {
+        println!("X is greater than Y");
+    } else if x < y {
+        println!("Y is greater than X");
+    } else {
+        println!("X and Y are equal");
+    }
+}
